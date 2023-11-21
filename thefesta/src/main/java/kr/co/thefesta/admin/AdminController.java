4b6a50a777f66fb1c023296d9a4607a1287433bd
hwang <hwang@hwang>
2023-11-21 16:03:21 +0900

    2023.11.21 HHJ admim_mapper and reportDTO and controller 수정

--- thefesta/src/main/java/kr/co/thefesta/admin/AdminController.java
+++ thefesta/src/main/java/kr/co/thefesta/admin/AdminController.java
@@ -20,10 +20,25 @@ public class AdminController {
 	@Autowired
 	public IAdminService service;
 	
+	//member 테이블 회원정보 list
 	@RequestMapping(value = "/memberList", method = RequestMethod.GET)
 	public List<MemberDTO> memberGet()throws Exception{
 		log.info("member Get....");
-		List<MemberDTO> memberList = service.memberList();
+		List<MemberDTO> memberList= service.memberList();
+		
+		//member statecode(회원상태)번호를 상태글자로 변경
+		for (MemberDTO memberStatecode : memberList) {
+			if(memberStatecode.getStatecode().equals("1")) {
+				memberStatecode.setStatecode("일반");
+			}else if(memberStatecode.getStatecode().equals("2")) {
+				memberStatecode.setStatecode("탈퇴");
+			}else if(memberStatecode.getStatecode().equals("3")) {
+				memberStatecode.setStatecode("재가입 가능");
+			}else {
+				memberStatecode.setStatecode("강퇴");
+			}
+		}
+		
 		log.info("memberList = " + memberList);
 		
 		return memberList;
---
+++ thefesta/src/main/java/kr/co/thefesta/admin/domain/ReportDTO.java
@@ -0,0 +1,8 @@
+package kr.co.thefesta.admin.domain;
+
+import java.util.Date;
+
+public class ReportDTO {
+	private String statecode;
+	private Date finalaccess;
+}
