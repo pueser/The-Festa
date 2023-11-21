33feeb577902f2416eb77a5352785cb5aa2e58e5
503-14 <503-14@DESKTOP-1IV7SR6>
2023-11-21 10:20:04 +0900

    2023.11.21 RSI Board_domain_update

---
+++ thefesta/src/main/java/kr/co/thefesta/board/domain/BoardImageDTO.java
@@ -0,0 +1,12 @@
+package kr.co.thefesta.board.domain;
+
+import lombok.Data;
+
+@Data
+public class BoardImageDTO {
+
+	private String bfileuuid;
+	private String bfileName;
+	private int bno;
+	
+}
