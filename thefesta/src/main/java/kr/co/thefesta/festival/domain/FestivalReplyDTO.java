c4576503c12273f5e59823245fdbf8948e91a48a
YWHee <ywh1390@naver.com>
2023-11-22 16:12:28 +0900

    2023.11.22 YWH festival_reply 수정

--- thefesta/src/main/java/kr/co/thefesta/festival/domain/FestivalReplyDTO.java
+++ thefesta/src/main/java/kr/co/thefesta/festival/domain/FestivalReplyDTO.java
@@ -2,6 +2,8 @@ package kr.co.thefesta.festival.domain;
 
 import java.sql.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,6 +19,10 @@ public class FestivalReplyDTO {
 	private String id;
 	private String nickname;
 	private String frcontent;
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
 	private Date frregist;
+	
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern = "yyyy.MM.dd HH:mm:ss")
 	private Date fredit;
 }
