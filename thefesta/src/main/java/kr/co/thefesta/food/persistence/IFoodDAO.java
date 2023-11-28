07d58982feaaeca650e2f6dfc2d5d2d4fdc7e038
PMR <12121db@gmail.com>
2023-11-28 22:36:30 +0900

    2023.11.28 PMR push 누락

--- thefesta/src/main/java/kr/co/thefesta/food/persistence/IFoodDAO.java
+++ thefesta/src/main/java/kr/co/thefesta/food/persistence/IFoodDAO.java
@@ -6,7 +6,4 @@ public interface IFoodDAO {
 	public void insert(ItemDTO itemDto) throws Exception;
 	public void delete() throws Exception;
 	
-//	List<Map<String, Object>> selectFood();
-//	void updateFood(Map<String, Object> food);
-	
 }
